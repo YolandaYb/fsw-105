b8152ea728aa4bdf9bb12e57cacc070cd879b631
YolandaYb <yolanda.ybarra@bryanuniversity.edu>
2020-06-07 13:42:50 -0400

    first

---
+++ week1/grocery_storeJS/index.js
@@ -0,0 +1,10 @@
+var shopper = {
+    firstName: "John",
+    lastName : "Doe",
+    item       : 6,
+    groceryCart = [ 'Cheese', 'Beef', 'Grains'], 
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  
